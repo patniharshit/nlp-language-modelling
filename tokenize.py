686f03559b31e7783602c0704b4e54420e2d0390
patniharshit <patniharshit@gmail.com>
2018-08-20 01:51:38 +0530

    detect grammar

--- tokenize.py
+++ tokenize.py
@@ -73,7 +73,84 @@ def spellcheck(language_models, word_to_check, n_grams):
 
     return word_to_check[:mini[2]] + correct_char + word_to_check[mini[2]+1:]
 
-def main():
+def create_language_model(words, n_grams):
+    language_model = defaultdict(list)
+    print("creating inverted index")
+    for i in range(len(words)-n_grams+1):
+        sliced = words[i:i+n_grams]
+        gram = " ".join(sliced[:-1])
+        next_word = sliced[-1]
+        if not gram:
+            gram = next_word
+        if gram in language_model:
+            language_model[gram].append(next_word)
+        else:
+            language_model[gram] = [next_word]
+
+    print("computing probablities")
+    for key in language_model.keys():
+        next_words = language_model[key]
+        unique_words = set(next_words)
+        nb_words = len(next_words)
+        probabilities_given_key = {}
+        for unique_word in unique_words:
+            probabilities_given_key[unique_word] = \
+                float(next_words.count(unique_word)) / nb_words
+        language_model[key] = probabilities_given_key
+    return language_model
+
+def language_model_for_grammar_detection(n_grams):
+    gutenberg_corpus = glob.glob('./Gutenberg/txt/F*')
+    words = []
+
+    print("Tokenizing corpus")
+    for book in gutenberg_corpus:
+        fp = open(book, "r")
+        contents = fp.read()
+        words += map(lambda x: x.strip().lower(), filter(lambda x: x is not None and x.isalpha() and len(x)!=0,
+                re.split(r'(\b[^\s]+\b)((?<=\.\w).)?', contents)))
+
+    models = []
+    print("Creating language models")
+    for n in range(n_grams+1)[:0:-1]:
+        print("create model", n)
+        models.append(create_language_model(words, n))
+        print("model created")
+
+    return models
+
+def rate_grammar(models, sentence, n_grams):
+    probablity = 1
+
+    for i in range(len(sentence)-n_grams+1):
+        gram = sentence[i:i+n_grams]
+        current_words = " ".join(gram[:-1])
+        next_word = gram[-1]
+        if not current_words:
+            current_words = next_word
+        curr_probab = 0
+        # multiple language_models
+        model_number = 0
+        while curr_probab == 0:
+            current_words = " ".join(current_words.split(" ")[1:])
+            if not current_words:
+                current_words = next_word
+            if models[model_number].get(current_words, None):
+                curr_probab = models[model_number][current_words].get(next_word, 0)
+            model_number += 1
+        probablity *= curr_probab
+
+    return probablity
+
+def main_grammar():
+    n_grams = 3
+    models = language_model_for_grammar_detection(n_grams)
+    sentences = [["he","is","the","king","of","this","place"], ["he","is","the","king","of","these","place"]]
+    for sentence in sentences:
+        print(sentence, rate_grammar(models, sentence, n_grams))
+    import pdb; pdb.set_trace()
+
+def main_spelling():
     gutenberg_corpus = glob.glob('./Gutenberg/txt/A*')
     word_frequency = Counter({})
     sentences = []
@@ -89,8 +166,8 @@ def main():
     n_grams = 4
     models = []
 
-    for n in range(n_grams)[:0:-1]:
-        models.append(create_character_model(word_frequency.keys(), n_grams))
+    for n in range(n_grams+1)[:0:-1]:
+        models.append(create_character_model(word_frequency.keys(), n))
 
     wrong_spellings = ["intelligemt", "apparantly", "calender", "definately", "dilemna", "prononciation", "schepule", "privilede", "occasionelly", "occasionalli", "occasiomally", "occassonally", "occasionatly"]
     for wrong_spelling in wrong_spellings:
@@ -99,4 +176,5 @@ def main():
 
 if __name__ == '__main__':
     # change terms to lowercase or stem them
-    main()
+    #main_spelling()
+    main_grammar()
