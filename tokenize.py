61844b2b2530740d9704b38139faa7564cca5eb3
patniharshit <patniharshit@gmail.com>
2018-08-19 14:11:25 +0530

    calculate probablities in character model

--- tokenize.py
+++ tokenize.py
@@ -1,63 +1,54 @@
 from __future__ import division
-from collections import Counter
+from collections import Counter, defaultdict
 import glob
 import re
 
-punctuations = re.compile(r"([,@#$%^&*()+={}\[\]|:;\"'<>~`'])")
-
-def tokenize():
-    pass
-
-def concat_delimiters(delimited_sentences):
-    sentences = []
-    for element in delimited_sentences:
-        if element:
-            if element.strip() not in ['.', '?', '!']:
-                sentences.append(element.strip())
-            else:
-                if sentences[-1]:
-                    if sentences[-1][-1] in ['.', '?', '!']:
-                        sentences[-1] = sentences[-1] + element
-                    else:
-                        sentences[-1] = sentences[-1] + " " + element
-    return sentences
-
 def tokenize_into_sentences(text):
-    text = re.sub(r"[\r\n]", r" ", text)
-    sentences = concat_delimiters(re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", text))
+    sentences = re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", text)
     return sentences
-
-def split_sentence(sentence):
-    return re.sub(punctuations, r" \1 ", sentence.strip()).split(" ")
 
 def tokenize_into_words(sentences):
     words = {}
     for sentence in sentences:
-        tokens = split_sentence(sentence)
-        for token in tokens:
-            if token:
-                if token in words:
-                    words[token] += 1
+        tokens = re.split(r'(\b[^\s]+\b)((?<=\.\w).)?', sentence)
+        for word in tokens:
+            if word and word.isalpha():
+                word = word.strip().lower()
+                if word in words:
+                    words[word] += 1
                 else:
-                    words[token] = 1
+                    words[word] = 1
     return words
 
 def create_character_model(words, n_grams):
-    character_model = {}
+    character_model = defaultdict(list)
     for word in words:
-        word = word + '$'
-        print(word)
+        word = '*' + word + '$'
         for i in range(len(word)-n_grams+1):
             sliced = word[i:i+n_grams]
-            if sliced in character_model:
-                character_model[sliced] += 1
+            gram = sliced[:-1]
+            next_char = sliced[-1]
+            if gram in character_model:
+                character_model[gram].append(next_char)
             else:
-                character_model[sliced] = 1
+                character_model[gram] = [next_char]
+
+    for key in character_model.keys():
+        next_chars = character_model[key]
+        unique_chars = set(next_chars) # removes duplicated
+        nb_chars = len(next_chars)
+        probabilities_given_key = {}
+        for unique_char in unique_chars:
+            probabilities_given_key[unique_char] = \
+                float(next_chars.count(unique_char)) / nb_chars
+        character_model[key] = probabilities_given_key
+    import pdb; pdb.set_trace()
     return character_model
 
 def spellcheck(words, word_to_check):
     character_model = create_character_model(words, 3)
     import pdb; pdb.set_trace()
+    return character_model
 
 def main():
     gutenberg_corpus = glob.glob('./Gutenberg/txt/*')
@@ -68,17 +59,15 @@ def main():
         fp = open(book, "r")
         contents = fp.read()
 
-        print(tokenize_into_sentences("""Mr. Smith bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't."""))
-        break
         sentence_arr = tokenize_into_sentences(contents)
         sentences += sentence_arr
         word_dict = tokenize_into_words(sentence_arr)
         word_frequency = word_frequency + Counter(word_dict)
-
-        spellcheck(word_frequency.keys(), "harshit")
+        print(spellcheck(word_frequency.keys(), "harshit"))
         # remove inverted commas in sentences of the form "[a-z]+\."
         #contents = re.sub(r"\"", r"", contents)
         #assert (len(re.findall(r"\".*\.\"", contents)) == 0)
+        break
 
 if __name__ == '__main__':
     # change terms to lowercase or stem them
